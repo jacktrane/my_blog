ab7d53e5e14c1cf69aad174e902833a3f7f4a865
jacktrane <jacktrane@163.com>
2016-05-12 00:44:24 +0800

    orm models OK

---
+++ www/test.py
@@ -0,0 +1,15 @@
+import orm,asyncio
+from models import User, Blog, Comment
+@asyncio.coroutine
+def test():
+  yield from orm.create_pool(user='root', password='root', database='my_blog')
+
+  u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+  yield from u.save()
+
+# for x in test():
+#     pass
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test())
+loop.close()
